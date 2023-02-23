cbbd95113bc9df0ad8a500e39c319fe36ea6403b
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-02-23 14:16:34 +0100

    Level: easy

---
+++ FruitBowl/src/FruitBowl.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FruitBowl {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int totalFruit = scanner.nextInt();
+        int Apples = totalFruit / 2;
+        int Bananas = totalFruit / 2;
+        Apples = Bananas;
+        int Pies = Apples / 3;
+
+        System.out.println(Pies);
+    }
+}
