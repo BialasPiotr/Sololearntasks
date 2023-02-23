54c8f7003919ffe26018507c93046e7bfe955b48
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-02-23 13:28:57 +0100

    Level: Easy

---
+++ Popsicles/src/Popsicles.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Popsicles {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numSiblings = scanner.nextInt();
+        int numPopsicles = scanner.nextInt();
+
+        if (numPopsicles % numSiblings == 0) {
+            System.out.println("give away");
+        } else {
+            System.out.println("eat them yourself");
+        }
+
+        scanner.close();
+    }
+}
