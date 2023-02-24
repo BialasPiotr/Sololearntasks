2da6a0eedccb30248b5c8b3aef14bd35cf4a496d
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-02-24 10:39:29 +0100

    Level: Hard

---
+++ PasswordValidation/src/PasswordValidation.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PasswordValidation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter password to evaluate: ");
+        String password = scanner.nextLine();
+
+        int numCount = 0;
+        int specialCharCount = 0;
+
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (Character.isDigit(c)) {
+                numCount++;
+            } else if ("!@#$%&*".indexOf(c) != -1) {
+                specialCharCount++;
+            }
+        }
+
+        if (password.length() >= 7 && numCount >= 2 && specialCharCount >= 2) {
+            System.out.println("Strong");
+        } else {
+            System.out.println("Weak");
+        }
+    }
+}
