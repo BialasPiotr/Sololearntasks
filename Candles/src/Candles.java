00a4c05947616c4ecd72e4f8e2d2c700e98d1b8c
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-02-26 10:12:23 +0100

    Level: Easy

---
+++ Candles/src/Candles.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Candles{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many friends are joining your order? ");
+        int numFriends = sc.nextInt();
+
+        int totalCandles = (numFriends + 1) * 9;
+        System.out.println("You should order " + totalCandles + " candles in total for the holiday.");
+
+        sc.close();
+    }
+
+}
