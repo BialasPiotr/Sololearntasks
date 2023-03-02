b852311cdea88a1fe5786271bcd2540f401f9157
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-03-02 11:38:20 +0100

    Level: Easy

---
+++ Balconies/src/Balconies.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Balconies{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter dimensions of balcony A (length,width): ");
+        String balconyA = scanner.nextLine();
+        String[] partsA = balconyA.split(",");
+
+        double lengthA = Double.parseDouble(partsA[0]);
+
+        double widthA = Double.parseDouble(partsA[1]);
+
+        double areaA = lengthA * widthA;
+
+        System.out.print("Enter dimensions of balcony B (length,width): ");
+        String balconyB = scanner.nextLine();
+        String[] partsB = balconyB.split(",");
+
+        double lengthB = Double.parseDouble(partsB[0]);
+
+        double widthB = Double.parseDouble(partsB[1]);
+
+        double areaB = lengthB * widthB;
+
+        if (areaA > areaB) {
+            System.out.println("Apartment A");
+        } else if (areaB > areaA) {
+            System.out.println("Apartment B");
+        } else {
+            System.out.println("Both balconies have the same area.");
+        }
+    }
+}
