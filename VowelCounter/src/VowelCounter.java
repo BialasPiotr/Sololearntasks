95bb4b62f5519244c07991e1f054efe770f2aad7
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-03-04 22:09:01 +0100

    Level: Easy

---
+++ VowelCounter/src/VowelCounter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class VowelCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        input = input.toLowerCase();
+
+        int vowelCount = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowelCount++;
+            }
+        }
+
+        System.out.println("The total number of vowels in the string is: " + vowelCount);
+    }
+}
