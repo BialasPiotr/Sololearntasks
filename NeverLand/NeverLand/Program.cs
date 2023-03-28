a5d0e09f616de9a5b25920fbf28bc050fd6154a1
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-03-28 10:34:59 +0200

    Level Easy C#

---
+++ NeverLand/NeverLand/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Neverland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.Write("Enter your age when you arrived at Neverland: ");
+            int myAge = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of years that have passed since your twin went back: ");
+            int yearsPassed = int.Parse(Console.ReadLine());
+            int twinAge = myAge + yearsPassed;
+            int ageDifference = twinAge - myAge;
+            Console.WriteLine("My twin is {0} years old and He is {1} years older than me", twinAge, ageDifference);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
