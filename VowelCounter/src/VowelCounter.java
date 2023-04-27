02b002da472642430a1984e691ef5dd300438653
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-04-27 12:30:23 +0200

    Update VowelCounter.java

--- VowelCounter/src/VowelCounter.java
+++ VowelCounter/src/VowelCounter.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class VowelCounter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
         input = input.toLowerCase();
