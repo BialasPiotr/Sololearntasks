d71f73579125f2ad7206426a5c21114e45502f78
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-06-01 22:09:59 +0200

    Update DutyFree.java

--- DutyFree/src/DutyFree.java
+++ DutyFree/src/DutyFree.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class DutyFree {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] input = scanner.nextLine().split(" ");
 
